7391eb88cf646f4be95ed72f874b7b86f2a2b0e2
Oliver Wahlen <oliver.wahlen@infinit-group.de>
2022-04-02 13:20:16 +0200

    batched pr query

--- user-prs.py
+++ user-prs.py
@@ -1,6 +1,7 @@
-import requests
 import configparser
 import pprint
+
+import requests
 
 config = configparser.ConfigParser()
 config.read('github.ini')
@@ -8,56 +9,86 @@ config.read('github.ini')
 token = config['GITHUB']['PersonalAccessToken']
 org = config['GITHUB']['Organization']
 
-userQuery = """
-{ 
-  organization(login: "%s"){
-    membersWithRole(first: 1000){
-      nodes {
-        login
-      }
-    }
-  }
-}
-"""
-pullRequestQuery = """
-  %s: search(query: "org:%s author:%s type:pr is:merged", type: ISSUE){
-    issueCount
-  } 
-"""
+
+def batch(iterable, n=1):
+    l = len(iterable)
+    for ndx in range(0, l, n):
+        yield iterable[ndx:min(ndx + n, l)]
+
 
 def makeCall(query):
     response = requests.post("https://api.github.com/graphql",
-        headers = {
-            "Authorization": f"Bearer {token}"
-        },
-        json = {
-            "query": query
-        }
-    )
+                             headers={
+                                 "Authorization": f"Bearer {token}"
+                             },
+                             json={
+                                 "query": query
+                             }
+                             )
     json = response.json()
-    if json['errors']:
+    if 'errors' in json:
+        print("Github response contains errors:")
         pprint.pprint(json['errors'])
         exit(-1)
     else:
         return json["data"]
 
-userList = makeCall(userQuery % org)
 
-#build a list of keys matching user (user0: johndoe ....)
-keyList = {}
-for idx, user in enumerate(userList["organization"]["membersWithRole"]["nodes"]):
-    keyList[f"user{idx}"] = user["login"]
+def getOrgMembers(org):
+    userQuery = """
+    { 
+      organization(login: "%s") {
+        membersWithRole(%s) {
+          pageInfo {
+            startCursor
+            hasNextPage
+            endCursor
+          }
+          nodes {
+            login
+          }
+        }
+      }
+    }
+    """
+    nodes = []
+    membersWithRoleArgs = "first: 100"
+    hasNextPage = True
+    while hasNextPage:
+        page = makeCall(userQuery % (org, membersWithRoleArgs))
+        nodes.extend(page["organization"]["membersWithRole"]["nodes"])
+        hasNextPage = page["organization"]["membersWithRole"]["pageInfo"]["hasNextPage"]
+        endCursor = page["organization"]["membersWithRole"]["pageInfo"]["endCursor"]
+        membersWithRoleArgs = 'first: 100, after: "%s"' % endCursor
+    return nodes
 
-#build the query
-builtQuery = "".join([
-    pullRequestQuery % (t, org, keyList[t]) 
-    for t in keyList.keys()
-])
-result = makeCall("{%s}" % builtQuery)
 
-#match the original user login
-stats = {}
-for it in result.keys():
-    stats[keyList[it]] = result[it]["issueCount"]
+def getPrCountDict(org, users):
+    pullRequestQuery = """
+    %s: search(query: "org:%s author:%s type:pr is:merged created:>=2022-03-01 created:<2022-04-01", type: ISSUE) {
+      issueCount
+    } 
+    """
+    keyDict = {}
+    for idx, user in enumerate(users):
+        keyDict[f"user{idx}"] = user["login"]
 
-print(stats)
+    keys = list(keyDict.keys())
+    for keyBatch in batch(keys, 10):
+        composedQuery = "".join([
+            pullRequestQuery % (t, org, keyDict[t])
+            for t in keyBatch
+        ])
+        result = makeCall("{%s}" % composedQuery)
+        print(result)
+
+
+users = getOrgMembers(org)
+prCounts = getPrCountDict(org, users)
+
+# match the original user login
+# stats = {}
+# for it in result.keys():
+#     stats[keyList[it]] = result[it]["issueCount"]
+#
+# print(stats)
