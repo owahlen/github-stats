add903fbcaea1a96a7816471635fc0304de1a939
Oliver Wahlen <oliver.wahlen@infinit-group.de>
2022-04-02 23:06:14 +0200

    added excel converter for visualization

---
+++ visualize-user-prs.py
@@ -0,0 +1,22 @@
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+
+import openpyxl
+import pandas as pd
+
+with open('user-prs.json', 'r') as f:
+    data = json.loads(f.read())
+
+flattened_data = []
+for d in data:
+    new_record = {
+        "saml_email":d["saml_email"]
+    }
+    for m in d["pr_history"]:
+        new_record[m["month"]] = m["n_merged"]
+    flattened_data.append(new_record)
+
+df = pd.json_normalize(flattened_data)
+df = df.set_index('saml_email')
+df.to_excel("prs.xlsx")
